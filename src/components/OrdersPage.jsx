dadeb8833a696e7265f71306cf73c02b54888fc2
CatoReinante <catoreinante@gmail.com>
2025-07-30 10:45:52 -0300

    dashboard

--- src/components/OrdersPage.jsx
+++ src/components/OrdersPage.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function OrdersPage() {
-  return <div>Order</div>;
+  return (
+    <div className="container-fluid">
+      <div className="row mt-4 alturaOverview">
+        <div className="col border rounded shadow me-3 p-4 ">
+          <h3>Algo</h3>
+        </div>
+        <div className="col border rounded shadow p-4">ESCRIBIR ACA</div>
+      </div>
+      <div className="row mt-4 alturaOverview">
+        <div className="col border rounded shadow me-3 p-4">ESCRIBIR ACA</div>
+        <div className="col border rounded shadow me-3 p-4">ESCRIBIR ACA</div>
+        <div className="col border rounded shadow p-4">ESCRIBIR ACA</div>
+      </div>
+    </div>
+  );
 }
 
 export default OrdersPage;
--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -6,18 +6,50 @@ function Overview() {
       <div className="row mt-4 alturaOverview">
         <div className="col border rounded shadow me-3 p-4 ">
           <h3>Algo</h3>
-          <img
-            src="src\img\fotoGrafifca.png"
-            alt=""
-            className="img-fluid rounded"
-          />
         </div>
         <div className="col border rounded shadow p-4">ESCRIBIR ACA</div>
       </div>
       <div className="row mt-4 alturaOverview">
         <div className="col border rounded shadow me-3 p-4">
-          <h3>Customers</h3>
-          ESCRIBIR ACA
+          <h3>Orders</h3>
+          <div className="row">
+            <div className="col">
+              <ul>
+                <li>Order 1</li>
+                <li>Order 2</li>
+                <li>Order 3</li>
+                <li>Order 4</li>
+                <li>Order 5</li>
+              </ul>
+            </div>
+            <div className="col">
+              <ul>
+                <li>Name 1</li>
+                <li>Name 2</li>
+                <li>Name 3</li>
+                <li>Name 4</li>
+                <li>Name 5</li>
+              </ul>
+            </div>
+            <div className="col">
+              <ul>
+                <li>Pending</li>
+                <li>Shipped</li>
+                <li>Delivered</li>
+                <li>Cancelled</li>
+                <li>Returned</li>
+              </ul>
+            </div>
+            <div className="col">
+              <ul>
+                <li>$100</li>
+                <li>$200</li>
+                <li>$300</li>
+                <li>$400</li>
+                <li>$500</li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div className="col border rounded shadow me-3 p-4">
           <h3>Analytics</h3>
