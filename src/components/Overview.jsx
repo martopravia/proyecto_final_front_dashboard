662e5134cb8dcfb84d50a7f799467875d9e3daeb
CatoReinante <catoreinante@gmail.com>
2025-07-30 09:56:58 -0300

    Principios de dashboard

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -2,8 +2,49 @@ import React from "react";
 
 function Overview() {
   return (
-    <div className="container">
+    <div className="container mt-3">
       <h1>Overview</h1>
+      <div className="row mt-3">
+        <div className="col border rounded shadow me-3 p-4">
+          <h3>Orders</h3>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+          necessitatibus, labore maiores quia quam est? Maxime, earum? Hic
+          perspiciatis ut sed est distinctio magnam iure reiciendis, libero qui
+          facilis pariatur aut alias earum assumenda doloribus eveniet tenetur
+          nesciunt, culpa ad.Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Alias necessitatibus, labore maiores quia quam est? Maxime,
+          earum? Hic perspiciatis ut sed est distinctio magnam iure reiciendis,
+          libero qui facilis pariatur aut alias earum assumenda doloribus
+          eveniet tenetur nesciunt, culpa ad.Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Alias necessitatibus, labore maiores
+          quia quam est? Maxime, earum? Hic perspiciatis ut sed est distinctio
+          magnam iure reiciendis, libero qui facilis pariatur aut alias earum
+          assumenda doloribus eveniet tenetur nesciunt, culpa ad.Lorem ipsum
+          dolor sit amet, consectetur adipisicing elit. Alias necessitatibus,
+          labore maiores quia quam est? Maxime, earum? Hic perspiciatis ut sed
+          est distinctio magnam iure reiciendis, libero qui facilis pariatur aut
+          alias earum assumenda doloribus eveniet tenetur nesciunt, culpa ad.
+        </div>
+        <div className="col border rounded shadow p-4"></div>
+      </div>
+      <div className="row mt-3">
+        <div className="col border rounded shadow me-3 p-4">
+          <h3>Customers</h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sequi
+          beatae culpa? Deserunt accusantium iusto odit doloribus, delectus
+          veritatis adipisci in totam atque quod fugiat aliquid soluta expedita
+          rem ipsum quidem? Reiciendis, dolorem. Quia cumque distinctio fuga in
+          beatae, sit cupiditate. voluptas odit dolore accusamus qui saepe
+          obcaecati consectetur fugit beatae, sit cupiditate. beatae, sit
+          cupiditate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </div>
+        <div className="col border rounded shadow me-3 p-4">
+          <h3>Analytics</h3>
+        </div>
+        <div className="col border rounded shadow me-3 p-4">
+          <h3>Sales?</h3>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 function SideBar() {
   return (
@@ -17,99 +17,85 @@ function SideBar() {
           height="32"
           aria-hidden="true"
         ></svg>
-        <span className="fs-4">Sidebar</span>
+        <span className="fs-4 ">Sidebar</span>
       </a>
-      <ul className="nav nav-pills flex-column mb-auto">
+      <hr />
+      <ul className="nav nav-pills flex-column mb-auto ms-5">
         <li className="nav-item">
-          <Link to={"orders"}> Orders</Link>
+          <NavLink
+            to={""}
+            style={{ color: "white", textDecoration: "none" }}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Overview
+          </NavLink>
         </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <svg
-              className="bi pe-none me-2"
-              width="16"
-              height="16"
-              aria-hidden="true"
-            ></svg>
-            Dashboard
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <svg
-              className="bi pe-none me-2"
-              width="16"
-              height="16"
-              aria-hidden="true"
-            ></svg>
+        <hr />
+        <li className="nav-item">
+          <NavLink
+            to={"orders"}
+            style={{ color: "white", textDecoration: "none" }}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
             Orders
-          </a>
+          </NavLink>
         </li>
+        <hr />
         <li>
-          <a href="#" className="nav-link text-white">
-            <svg
-              className="bi pe-none me-2"
-              width="16"
-              height="16"
-              aria-hidden="true"
-            ></svg>
+          <NavLink
+            to={"products"}
+            style={{ color: "white", textDecoration: "none" }}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
             Products
-          </a>
+          </NavLink>
         </li>
+        <hr />
         <li>
-          <a href="#" className="nav-link text-white">
-            <svg
-              className="bi pe-none me-2"
-              width="16"
-              height="16"
-              aria-hidden="true"
-            ></svg>
+          <NavLink
+            to={"customers"}
+            style={{ color: "white", textDecoration: "none" }}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
             Customers
-          </a>
+          </NavLink>
+        </li>
+        <hr />
+        <li>
+          <NavLink
+            to={"analytics"}
+            style={{ color: "white", textDecoration: "none" }}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Analytics
+          </NavLink>
+        </li>
+        <hr />
+        <li>
+          <NavLink
+            to={"settings"}
+            style={{ color: "white", textDecoration: "none" }}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Settings
+          </NavLink>
         </li>
       </ul>
-      <div className="dropdown">
-        <a
-          href="#"
-          className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <img
-            src="https://github.com/mdo.png"
-            alt=""
-            width="32"
-            height="32"
-            className="rounded-circle me-2"
-          />
-          <strong>mdo</strong>
-        </a>
-        <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-          <li>
-            <a className="dropdown-item" href="#">
-              New project...
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Settings
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Profile
-            </a>
-          </li>
-          <li>
-            <hr className="dropdown-divider" />
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Sign out
-            </a>
-          </li>
-        </ul>
-      </div>
+
+      <hr />
+      <button className="btn btn-outline-light">Log out</button>
     </div>
   );
 }
