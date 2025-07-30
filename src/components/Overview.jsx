cedb9b1a8462f84cb9457f24464efc6dc7e22561
CatoReinante <catoreinante@gmail.com>
2025-07-30 10:27:20 -0300

    comienzo dashboard

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -2,47 +2,30 @@ import React from "react";
 
 function Overview() {
   return (
-    <div className="container mt-3">
-      <h1>Overview</h1>
-      <div className="row mt-3">
-        <div className="col border rounded shadow me-3 p-4">
-          <h3>Orders</h3>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
-          necessitatibus, labore maiores quia quam est? Maxime, earum? Hic
-          perspiciatis ut sed est distinctio magnam iure reiciendis, libero qui
-          facilis pariatur aut alias earum assumenda doloribus eveniet tenetur
-          nesciunt, culpa ad.Lorem ipsum dolor sit amet, consectetur adipisicing
-          elit. Alias necessitatibus, labore maiores quia quam est? Maxime,
-          earum? Hic perspiciatis ut sed est distinctio magnam iure reiciendis,
-          libero qui facilis pariatur aut alias earum assumenda doloribus
-          eveniet tenetur nesciunt, culpa ad.Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Alias necessitatibus, labore maiores
-          quia quam est? Maxime, earum? Hic perspiciatis ut sed est distinctio
-          magnam iure reiciendis, libero qui facilis pariatur aut alias earum
-          assumenda doloribus eveniet tenetur nesciunt, culpa ad.Lorem ipsum
-          dolor sit amet, consectetur adipisicing elit. Alias necessitatibus,
-          labore maiores quia quam est? Maxime, earum? Hic perspiciatis ut sed
-          est distinctio magnam iure reiciendis, libero qui facilis pariatur aut
-          alias earum assumenda doloribus eveniet tenetur nesciunt, culpa ad.
+    <div className="container-fluid">
+      <div className="row mt-4 alturaOverview">
+        <div className="col border rounded shadow me-3 p-4 ">
+          <h3>Algo</h3>
+          <img
+            src="src\img\fotoGrafifca.png"
+            alt=""
+            className="img-fluid rounded"
+          />
         </div>
-        <div className="col border rounded shadow p-4"></div>
+        <div className="col border rounded shadow p-4">ESCRIBIR ACA</div>
       </div>
-      <div className="row mt-3">
+      <div className="row mt-4 alturaOverview">
         <div className="col border rounded shadow me-3 p-4">
           <h3>Customers</h3>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sequi
-          beatae culpa? Deserunt accusantium iusto odit doloribus, delectus
-          veritatis adipisci in totam atque quod fugiat aliquid soluta expedita
-          rem ipsum quidem? Reiciendis, dolorem. Quia cumque distinctio fuga in
-          beatae, sit cupiditate. voluptas odit dolore accusamus qui saepe
-          obcaecati consectetur fugit beatae, sit cupiditate. beatae, sit
-          cupiditate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          ESCRIBIR ACA
         </div>
         <div className="col border rounded shadow me-3 p-4">
           <h3>Analytics</h3>
+          ESCRIBIR ACA
         </div>
-        <div className="col border rounded shadow me-3 p-4">
+        <div className="col border rounded shadow p-4">
           <h3>Sales?</h3>
+          ESCRIBIR ACA
         </div>
       </div>
     </div>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,18 +7,7 @@ function SideBar() {
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark vh-100"
       style={{ width: "280px" }}
     >
-      <a
-        href="/"
-        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
-      >
-        <svg
-          className="bi pe-none me-2"
-          width="40"
-          height="32"
-          aria-hidden="true"
-        ></svg>
-        <span className="fs-4 ">Sidebar</span>
-      </a>
+      <span className="fs-4 text-center">Sidebar</span>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto ms-5">
         <li className="nav-item">
